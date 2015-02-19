2f58d3981e21165b715e841a5822c0c24ad4d2d1
Miranda Lv <zhonghui.lv@gmail.com>
2015-02-19 16:56:50 -0500

    add transaction_table.py which will be used to produce transaction table

---
+++ transation_table.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import json
+import sys
+import csv
+from pandas import DataFrame as df
+
+inf = r"/home/mirandalv/repos/nepal-geocoded-dataset/scratch1/transation/ComDis_NP-csv.tsv"
+csvoutput = r"/home/mirandalv/repos/nepal-geocoded-dataset/scratch1/transation/transation_NP.csv"
+
+def get_fields(incsv):
+	rawData = csv.DictReader(open(inf, 'rb'), delimiter='\t')
+	header=rawData.fieldnames
+	return header
+
+rawData = csv.DictReader(open(inf, 'rb'), delimiter='\t')
+fields = rawData.fieldnames
+c_dfields = fields[2:]
+
+newheader = ["project_id", "project_title", "transaction_year", "transaction_value_code", "transaction_value"]
+
+
+indicate = []
+
+for row in rawData:
+	for field in c_dfields:	
+		if str(row[field]) != str(0):
+			newrow = {}
+			trans_val_code = field.split("_")[0]
+			trans_year = field.split("_")[1]
+			newrow["project_id"] = row["AMP ID"]
+			newrow["project_title"] = row["Project Title"]
+			newrow["transaction_year"] = trans_year
+			newrow["transaction_value_code"] = trans_val_code
+			newrow["transaction_value"] = row[field]
+			indicate.append(newrow)
+
+
+
+outdf=df(indicate, columns=newheader)
+outdf.to_csv(csvoutput, sep='\t', quotechar='\"', encoding='utf-8')
+
+
+
+
+
